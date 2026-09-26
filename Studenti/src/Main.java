c4612b80dd358656adb7f5c7bc25546dab8e30bc
rosedefloris <rosedefloris@gmail.com>
2026-09-26 08:14:27 +0200

    esecuzione es studenti

--- Studenti/src/Main.java
+++ Studenti/src/Main.java
@@ -1,10 +1,6 @@
 
 public class Main {
     public static void main (String[] args) {
-        Studenti s;
-        s = new Studenti();
-        s.nome = "Ada";
-        Studenti t;
-        t = s;
+
     }
 }
